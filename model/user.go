5fd515db9c009585cde9e3c58d23e36bc21f854c
Maximilian Falk <falk.maximilian@outlook.com>
2024-04-27 16:12:21 +0200

    Json in model

--- model/user.go
+++ model/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"uniqueIndex;not null;size:50;" validate:"required;min=3;max=50"`
-	Email    string `gorm:"uniqueIndex;not null;size:255;" validate:"required,email"`
-	Password string `gorm:"not null;" validate:"required"`
+	Username string `gorm:"uniqueIndex;not null;size:50;" validate:"required;min=3;max=50" json:"username"`
+	Email    string `gorm:"uniqueIndex;not null;size:255;" validate:"required,email" json:"email"`
+	Password string `gorm:"not null;" validate:"required" json:"password"`
 }
